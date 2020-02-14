632f4915ddda78530a2dc7797983f4b68d939251
Ben McNulty <benmcnulty@live.com>
2020-02-14 17:48:09 -0500

    fixes naming consistency and makes self-invoking

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,30 +1,32 @@
-// turn on JavaScript class
-document.documentElement.className = 
-document.documentElement.className.replace("no-js","js");
+(function(){
+  // turn on JavaScript class
+  document.documentElement.className = 
+  document.documentElement.className.replace("no-js","js");
 
-// initiate variables
-let touchType = ('ontouchstart' in window) ? "touchstart" : "click";
-let base = document.body;
-let modes = ["light", "dark", "color"];
-let lastIndex = modes.length - 1;
-let modeIndex = 0;
-let mode = modes[modeIndex];
-let example_button = document.getElementById("example-button");
+  // initiate variables
+  let touchType = ('ontouchstart' in window) ? "touchstart" : "click";
+  let body = document.body;
+  let modes = ["light", "dark", "color"];
+  let lastIndex = modes.length - 1;
+  let modeIndex = 0;
+  let mode = modes[modeIndex];
+  let example_button = document.getElementById("example-button");
 
-// define functions
-function cycleMode() {
-  base.classList.remove(mode);
-  if (modeIndex < lastIndex) {
-    modeIndex = modeIndex + 1;
-  } else {
-    modeIndex = 0;
+  // define functions
+  function cycleMode() {
+    body.classList.remove(mode);
+    if (modeIndex < lastIndex) {
+      modeIndex = modeIndex + 1;
+    } else {
+      modeIndex = 0;
+    }
+    mode = modes[modeIndex];
+    body.classList.add(mode);
   }
-  mode = modes[modeIndex];
-  base.classList.add(mode);
-}
 
-// set defaults
-base.classList.add(mode);
+  // set defaults
+  body.classList.add(mode);
 
-// attach events
-example_button.addEventListener(touchType, cycleMode);
+  // attach events
+  example_button.addEventListener(touchType, cycleMode);
+})();
